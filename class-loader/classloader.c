4fd53930b2776c7b0b7cdbd949fb34de63d5e170
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2014-12-09 00:39:21 +0000

    Commit some stuff.

---
+++ class-loader/classloader.c
@@ -0,0 +1,13 @@
+/*
+	A simple Java class loader.
+	180 degrees project: Day 1
+	Copyright (c) 2014 Sam Saint-Pettersen
+*/
+
+#include <stdio.h>
+#include "class.h"
+
+int main() {
+
+	return 0;
+}
---
+++ class-loader/struct.h
@@ -0,0 +1,23 @@
+/*
+	Define a Java class file.
+	180 degrees project: Day 1
+	Copyright (c) 2014 Sam Saint-Pettersen
+*/
+struct ClassFile  {
+	unsigned int magic; 				// u4: 0xCAFEBABE
+	unsigned short minor_version; 		// u2
+	unsigned short major_version;		// u2
+	unsigned short constant_pool_count;	// u2
+	/*cp_info constant_pool_count[constant_pool_count-1];
+	unsigned int access_flags;
+	unsigned int this_class;
+	unsigned int super_class;
+	unsigned int interfaces_count;
+	unsigned int interfaces[interfaces_count];
+	unsigned int fields_count;
+	field_info fields[fields_count];
+	unsigned int methods_count;
+	method_info methods[methods_count];
+	unsigned int attributes_count;
+	attributes_info attributes[attributes_count];*/
+}
